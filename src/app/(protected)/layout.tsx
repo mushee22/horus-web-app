a346020f9058f0f72cbc415136ee4078c4c84b91
mushee22 <mmtmusheer@gmail.com>
2025-05-22 00:14:31 +0530

    refactor: Comment out authentication check in layout component

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,7 +1,6 @@
 
 import MobileNavBar from '@/components/layout/mobile-nav-bar'
 import AuthContextProvider from '@/context/auth-context'
-import { getSession } from '@/lib/session'
 import { PropsWithChildren } from 'react'
 
 
@@ -9,7 +8,7 @@ export default async function layout({ children }: PropsWithChildren) {
 
     // const pathname = usePathname()
 
-    const { isAuthenticated } = await getSession();
+    // const { isAuthenticated } = await getSession();
 
     // const isHideMobileNavBar = pathname.includes('community')
 
