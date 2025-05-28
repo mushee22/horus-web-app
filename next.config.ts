18d5675400289facb1eb9bfa58c3e5112927c044
mushee22 <mmtmusheer@gmail.com>
2025-05-28 22:26:55 +0530

    fix: Update S3 hostname in Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -11,7 +11,7 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: 'https',
-        hostname: 'horusbackend.s3.amazonaws.com'
+        hostname: 'horuslearn.s3.amazonaws.com'
       }
     ],
   }
