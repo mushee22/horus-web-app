87ae54aced71d0cf00d7e6aadafbf278a03a428a
mushee22 <mmtmusheer@gmail.com>
2025-08-01 10:33:27 +0530

    fix: Update password validation to require a minimum length of 1 character

--- src/lib/definition.ts
+++ src/lib/definition.ts
@@ -4,12 +4,7 @@ export const SigInFormSchema = z.object({
     email: z.string().email({ message: 'Please enter a valid email.' }).trim(),
     password: z
         .string()
-        .min(8, { message: 'Be at least 8 characters long' })
-        .regex(/[a-zA-Z]/, { message: 'Contain at least one letter.' })
-        .regex(/[0-9]/, { message: 'Contain at least one number.' })
-        .regex(/[^a-zA-Z0-9]/, {
-            message: 'Contain at least one special character.',
-        })
+        .min(1, { message: 'Password is required' })
         .trim(),
 })
 
