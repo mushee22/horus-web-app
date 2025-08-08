bbbdb18995e99f2348553016be75d9aff5cd8a57
mushee22 <mmtmusheer@gmail.com>
2025-08-08 12:09:57 +0530

    refactor(hooks): remove websocket functionality from use-community
    The websocket implementation was moved to a separate hook and is no longer needed in use-community. This simplifies the hook's responsibility to only handle community data fetching.

--- src/hook/use-community.tsx
+++ src/hook/use-community.tsx
@@ -1,16 +1,16 @@
-import { useAuthContext } from "@/context/auth-context";
+// import { useAuthContext } from "@/context/auth-context";
 import { fetcher } from "@/lib/fetch";
 import { Community, Response } from "@/type";
 import { useQuery } from "@tanstack/react-query";
 import { useParams } from "next/navigation";
-import useWs from "./use-ws";
+// import useWs from "./use-ws";
 
 export default function useCommunity() {
   const roomId = useParams().slug as string;
 
-  const { user } = useAuthContext();
+  // const { user } = useAuthContext();
 
-  const { data, isLoading, refetch } = useQuery<Response<Community[]>>({
+  const { data, isLoading, } = useQuery<Response<Community[]>>({
     queryKey: ["chat-list"],
     queryFn: () => {
       return fetcher("list-rooms/");
@@ -18,26 +18,26 @@ export default function useCommunity() {
     refetchOnMount: "always",
   });
 
-  const handleUpdatedMessage = (ev: MessageEvent) => {
-    const currentMessage = JSON.parse(ev.data);
-    if (currentMessage.type == "notification") {
-      refetch();
-    }
-  };
+  // const handleUpdatedMessage = (ev: MessageEvent) => {
+  //   const currentMessage = JSON.parse(ev.data);
+  //   if (currentMessage.type == "notification") {
+  //     refetch();
+  //   }
+  // };
 
-  const { socketRef } = useWs({
-    path: `notifications/${user?.user.id}`,
-    isEnabled: !!user,
-    onMessage: handleUpdatedMessage,
-    onClose: () => {},
-    onOpen: () => {},
-    onError: () => {},
-  });
+  // const { socketRef } = useWs({
+  //   path: `notifications/${user?.user.id}`,
+  //   isEnabled: !!user,
+  //   onMessage: handleUpdatedMessage,
+  //   onClose: () => {},
+  //   onOpen: () => {},
+  //   onError: () => {},
+  // });
 
   return {
     slug: roomId,
     data,
     isLoading,
-    socketRef
+    // socketRef
   };
 }
